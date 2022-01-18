36af81289697b5df505a60546055f03cbe23d2ca
RahulSahu <rahulsahu_vds@yahoo.com>
2022-01-18 16:21:02 +0530

    Spring boot Curd

---
+++ src/main/java/com/hackerrank/sample/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.hackerrank.sample.entity;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name="tbl_customer")
+@Setter
+@Getter
+@ToString
+public class Customer
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long age;
+
+    private String location;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable=false, updatable=false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private Date updatedAt;
+}
